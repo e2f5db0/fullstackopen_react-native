4b5aa76beb0f450b871c0d70f9e658312fe386ce
e2f5db0 <sami.toiskallio@protonmail.com>
2022-05-17 14:09:56 +0300

    Complete exercise 10.11

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
+import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
   return (
-    <>
+    <View>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
-    </>
-  )
-}
+    </View>
+  );
+};
 
 export default App;
---
+++ src/graphql/queries.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      edges {
+        node {
+          id
+          ownerAvatarUrl
+          fullName
+          description
+          language
+          stargazersCount
+          forksCount
+          reviewCount
+          ratingAverage
+        }
+      }
+    }
+  }
+`;
+
+export const GET_USERS = gql`
+  query {
+    users {
+      edges {
+        node {
+          username
+        }
+      }
+    }
+  }
+`;
